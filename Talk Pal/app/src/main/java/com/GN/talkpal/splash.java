85153a99d7ad36cd3acbd6d7343f69d549a9b1e7
GauravDawange <gauravDawange8485@gmail.com>
2025-01-03 11:04:07 +0530

    initial commit

---
+++ Talk Pal/app/src/main/java/com/GN/talkpal/splash.java
@@ -0,0 +1,65 @@
+package com.GN.talkpal;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.telephony.SmsManager;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splash extends AppCompatActivity {
+    ImageView logo;
+    TextView name, own1, own2;
+    Animation topAnim, bottomAnim;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.logoimg);
+        name = findViewById(R.id.logonameimg);
+        own1 = findViewById(R.id.ownone);
+        own2 = findViewById(R.id.owntwo);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo.setAnimation(topAnim);
+        name.setAnimation(bottomAnim);
+        own1.setAnimation(bottomAnim);
+        own2.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this,login.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
+
+
+
+
